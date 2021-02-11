fbd2bc93c8148600dd08c1fe7dcb6dba27937cc2
Kevin Earls <kearls@redhat.com>
2021-02-11 11:23:43 +0100

    Remove commented out code
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- startTraffic.sh
+++ startTraffic.sh
@@ -12,8 +12,6 @@ oc new-project ${TRAFFIC_GENERATOR_NAMESPACE}
 # We need to create the configmap first
 # Set duration to "0s" for traffic to never end.
 # Set RATE to number of operations per second or per time unit.  Default is per second, we can use something like 1/10s to run every 10 seconds
-
-## FIXME -  require from command line, or allow overriding?
 DURATION="2m"
 RATE="20/5s"
 cat ./traffic-generator/openshift/traffic-generator-configmap.yaml | DURATION="${DURATION}" ROUTE="$ROUTE" RATE="$RATE"  envsubst | oc apply -n ${TRAFFIC_GENERATOR_NAMESPACE} -f -
