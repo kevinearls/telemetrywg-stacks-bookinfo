307dab995aad78514dd894a1664e3f99046338e7
Kevin Earls <kearls@redhat.com>
2021-02-11 11:18:43 +0100

    Add traffic generator and Jaeger in-memory option
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- installBookinfo.sh
+++ installBookinfo.sh
@@ -3,47 +3,18 @@ set -x
 set +e
 
 export CONTROL_PLANE_NAMESPACE=istio-system
-export CONTROL_PLANE_NAME=istio-system
 export BOOKINFO_NAMESPACE=bookinfo
 export BOOKINFO_BASE="https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/"
 export BOOKINFO_YAML="${BOOKINFO_BASE}/platform/kube/bookinfo.yaml"
 export BOOKINFO_GATEWAY="${BOOKINFO_BASE}/networking/bookinfo-gateway.yaml"
 export BOOKINFO_DESTINATION_RULES="${BOOKINFO_BASE}/networking/destination-rule-all.yaml"
-export TRAFFIC_GENERATOR_NAMESPACE=traffic
 
-oc delete project ${CONTROL_PLANE_NAMESPACE} || true
-oc wait --for=delete namespace/${CONTROL_PLANE_NAMESPACE}  --timeout=120s
-oc new-project ${CONTROL_PLANE_NAMESPACE} || true
-set -e
-oc create -f service-mesh-control-plane-v2.yaml
-
-sleep 90
-start_time=`date +%s`
-set +e
-maxtime="5 minute"
-endtime=$(date -ud "$maxtime" +%s)
-while [[ $(date -u +%s) -le $endtime ]]
-do
-    STATUS=$(oc get smcp istio-system --namespace ${CONTROL_PLANE_NAMESPACE} | grep istio-system | awk '{print $3}')
-    echo control plane istio-system status: ${STATUS}
-    if [ "${STATUS}" == "InstallSuccessful" ] ; then
-        oc get smcp --namespace ${CONTROL_PLANE_NAMESPACE}
-        break;
-    fi
-    sleep 10
-done
-set -e
-
-# Make sure we haven't fallen thru the loop above because of time out.
+# Make sure we have a service mesh
 oc get smcp --namespace ${CONTROL_PLANE_NAMESPACE}
 oc get deployments --namespace ${CONTROL_PLANE_NAMESPACE}
 oc wait --for=condition=available deployment/istio-egressgateway --namespace ${CONTROL_PLANE_NAMESPACE}
 
-# Create the member roll
-oc apply --namespace ${CONTROL_PLANE_NAMESPACE} -f service-mesh-member-roll.yaml
-
-# Wait for the Jaeger instance to be ready
-sleep 60
+# Wait for the Jaeger instance to be ready -- FIXME
 export JAEGER_OPERATOR_NAMESPACE=$(oc get deployments --all-namespaces | grep jaeger-operator | awk '{print $1}')
 oc wait --for=condition=available deployment/jaeger-operator --namespace ${JAEGER_OPERATOR_NAMESPACE} --timeout=120s
 
@@ -62,7 +33,7 @@ oc apply -n ${BOOKINFO_NAMESPACE} -f ${BOOKINFO_GATEWAY}
 oc apply -n ${BOOKINFO_NAMESPACE} -f ${BOOKINFO_DESTINATION_RULES}
 
 export GATEWAY_URL=$(oc -n ${CONTROL_PLANE_NAMESPACE} get route istio-ingressgateway -o jsonpath='{.spec.host}')
-echo "The URL for the productpage is http://${GATEWAY_URL}/productpage"
+echo "The URL for the Bookinfo productpage is http://${GATEWAY_URL}/productpage"
 
 # TODO we either need to wait or retry here.Verify that it is installed -it should return a 200
 sleep 30
--- startTraffic.sh
+++ startTraffic.sh
@@ -1,15 +1,20 @@
-# This script will install the traffic generator.
-export ROUTE="http://"$(oc get route --namespace istio-system | grep bookinfo-bookinfo-gateway | awk '{print $2}')/productpage
-echo ROUTE is $ROUTE
+# This script will install and start the traffic generator.
 export TRAFFIC_GENERATOR_NAMESPACE=traffic
+export CONTROL_PLANE_NAMESPACE=istio-system
+
+export ROUTE="http://"$(oc get route --namespace ${CONTROL_PLANE_NAMESPACE} | grep bookinfo-bookinfo-gateway | awk '{print $2}')/productpage
+echo Bookinfo ROUTE is $ROUTE
 
 oc delete project ${TRAFFIC_GENERATOR_NAMESPACE} || true
 oc wait --for=delete namespace/${TRAFFIC_GENERATOR_NAMESPACE}  --timeout=120s
 oc new-project ${TRAFFIC_GENERATOR_NAMESPACE}
 
 # We need to create the configmap first
-# Set duration to "0s" for traffic to never end.  Set RATE to number of operations per second or per time unit.  Default is per second, we can use something like 1/10s to run every 10 seconds
+# Set duration to "0s" for traffic to never end.
+# Set RATE to number of operations per second or per time unit.  Default is per second, we can use something like 1/10s to run every 10 seconds
+
+## FIXME -  require from command line, or allow overriding?
 DURATION="2m"
 RATE="20/5s"
-curl https://raw.githubusercontent.com/kiali/kiali-test-mesh/master/traffic-generator/openshift/traffic-generator-configmap.yaml | DURATION="${DURATION}" ROUTE="$ROUTE" RATE="$RATE"  envsubst | oc apply -n ${TRAFFIC_GENERATOR_NAMESPACE} -f -
-curl https://raw.githubusercontent.com/kiali/kiali-test-mesh/master/traffic-generator/openshift/traffic-generator.yaml | oc apply -n ${TRAFFIC_GENERATOR_NAMESPACE} -f -
+cat ./traffic-generator/openshift/traffic-generator-configmap.yaml | DURATION="${DURATION}" ROUTE="$ROUTE" RATE="$RATE"  envsubst | oc apply -n ${TRAFFIC_GENERATOR_NAMESPACE} -f -
+cat ./traffic-generator/openshift/traffic-generator.yaml | oc apply -n ${TRAFFIC_GENERATOR_NAMESPACE} -f -
